f3b237250a4855dc8de9c70cf9c0e919cc574ee4
Matsio Dev Server <ag@matsio.com>
2025-08-14 07:04:16 +0000

    AS

--- api/submit/index.php
+++ api/submit/index.php
@@ -1,5 +1,9 @@
 <?php
 // Set the action parameter and route to main API
 $_GET['action'] = 'submit';
+
+// Debug: Log the request method
+error_log("Submit endpoint accessed with method: " . ($_SERVER['REQUEST_METHOD'] ?? 'UNKNOWN'));
+
 require_once '../index.php';
 ?>
